b73331b6a5b93c04cdc42caa8eb783e543771c03
Yoonseop Kang <e0engoon@gmail.com>
2017-07-27 22:54:00 +0900

    Serialize retrieved OAuth token and re-use it

--- tumblr_client.py
+++ tumblr_client.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from requests_oauthlib import OAuth1Session, OAuth1
-from secrets import client_id, client_secret
+import requests
+import pickle
+import os
+from secrets import CLIENT_ID, CLIENT_SECRET, OAUTH_FILE_NAME
 
 
 def oauth_url(name):
@@ -13,26 +16,38 @@ authorize_base_url = oauth_url("authorize")
 access_token_url = oauth_url("access_token")
 
 
-tumblr = OAuth1Session(client_id, client_secret=client_secret)
-tumblr.fetch_request_token(request_token_url)
+def get_oauth_tokens(client_id, client_secret):
+    tumblr = OAuth1Session(client_id, client_secret=client_secret)
+    tumblr.fetch_request_token(request_token_url)
 
-authorization_url = tumblr.authorization_url(authorize_base_url)
-print("Please go here and authorize.", authorization_url)
+    authorization_url = tumblr.authorization_url(authorize_base_url)
+    print("Please go here and authorize.", authorization_url)
 
-redirect_response = input("Pase the full redirect url:")
-tumblr.parse_authorization_response(redirect_response)
+    redirect_response = input("Pase the full redirect url:")
+    tumblr.parse_authorization_response(redirect_response)
 
-oauth_tokens = tumblr.fetch_access_token(access_token_url)
+    oauth_tokens = tumblr.fetch_access_token(access_token_url)
 
-resource_owner_key = oauth_tokens.get("oauth_token")
-resource_owner_secret = oauth_tokens.get("oauth_token_secret")
+    resource_owner_key = oauth_tokens.get("oauth_token")
+    resource_owner_secret = oauth_tokens.get("oauth_token_secret")
 
-oauth = OAuth1(client_id,
-               client_secret=client_secret,
-               resource_owner_key=resource_owner_key,
-               resource_owner_secret=resource_owner_secret)
+    tumblr.close()
+
+    return OAuth1(client_id,
+                  client_secret=client_secret,
+                  resource_owner_key=resource_owner_key,
+                  resource_owner_secret=resource_owner_secret)
+
+
+if os.path.exists(OAUTH_FILE_NAME):
+    with open(OAUTH_FILE_NAME, "rb") as fp:
+        oauth1 = pickle.load(fp)
+else:
+    oauth1 = get_oauth_tokens(CLIENT_ID, CLIENT_SECRET)
+    with open(OAUTH_FILE_NAME, "wb") as fp:
+        pickle.dump(oauth1, fp)
 
 api_url_prefix = "http://api.tumblr.com/v2/"
 
-r = tumblr.get(api_url_prefix + "user/likes")
+r = requests.get(api_url_prefix + "user/likes", auth=oauth1)
 print(r.content)
